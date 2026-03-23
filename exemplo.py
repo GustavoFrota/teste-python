def533dd297a5e25c2acfa883438aae0aaade783
GustavoFrota <gustavofro90@gmail.com>
2026-03-23 09:00:41 -0300

    Pequenas alterações

--- exemplo.py
+++ exemplo.py
@@ -1,7 +1,7 @@
-nome = input("Digite o seu nome")
+nome = input("Digite o seu nome: ")
 idade = 18
 
 if idade >= 18:
-    print("Maior de idade")
+    print(f'{nome} é maior de idade.')
 else:
-    print("Menor de idade")
+    print(f'{nome} é menor de idade.')
