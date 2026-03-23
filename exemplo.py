b974cdc185f3c4996bc293990a34288b21146c74
GustavoFrota <gustavofro90@gmail.com>
2026-03-23 13:35:06 -0300

    Pequena adição

--- exemplo.py
+++ exemplo.py
@@ -2,6 +2,8 @@ def identificacao():
     nome = input("Digite o seu nome: ")
     idade = int(input("Digite sua idade: "))
 
+    print(f'Olá, me chamo {nome} e eu tenho {idade} anos')
+
     if idade >=18:
         print(f'{nome} é maior de idade.')
     else:
