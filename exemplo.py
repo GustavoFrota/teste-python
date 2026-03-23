3775445751482a152c4e15317dcddd7873493b85
GustavoFrota <gustavofro90@gmail.com>
2026-03-23 13:41:46 -0300

    Adicionando NomeCompleto

--- exemplo.py
+++ exemplo.py
@@ -1,8 +1,12 @@
 def identificacao():
     nome = input("Digite o seu nome: ")
+    sobrenome = input("Digite o seu sobrenome: ")
+
+    NomeCompleto = nome + sobrenome
+
     idade = int(input("Digite sua idade: "))
 
-    print(f'Olá, me chamo {nome} e eu tenho {idade} anos')
+    print(f'Olá, me chamo {NomeCompleto} e eu tenho {idade} anos')
 
     if idade >=18:
         print(f'{nome} é maior de idade.')
