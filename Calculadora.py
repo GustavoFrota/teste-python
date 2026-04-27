bf9eac5b49fe278b40c822e3de47c02f354bfa22
GustavoFrota <gustavofro90@gmail.com>
2026-04-27 09:26:59 -0300

    Adicionando números decimais

--- Calculadora.py
+++ Calculadora.py
@@ -1,6 +1,6 @@
-n1 = int(input("Digite o primeiro número: "))
+n1 = float(input("Digite o primeiro número: "))
 operacao = input("Digite a operação: ")
-n2 = int(input("Digite o segundo número: "))
+n2 = float(input("Digite o segundo número: "))
 
 
 match operacao:
