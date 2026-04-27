5e57349f4ab56495fe208f6b68f112686fc702e2
GustavoFrota <gustavofro90@gmail.com>
2026-04-27 09:06:03 -0300

    Adicionando melhorias

--- Calculadora.py
+++ Calculadora.py
@@ -1,21 +1,26 @@
 n1 = int(input("Digite o primeiro número: "))
-n2 = int(input("Digite o segundo número: "))
 resultado = input("Digite a operação: ")
+n2 = int(input("Digite o segundo número: "))
+
 
 match resultado:
     case "+":
         resultado = n1 + n2
+        print(f'A soma de {n1} + {n2} é {resultado}')
     case "-":
         resultado = n1 - n2
+        print(f'A subtração de {n1} - {n2} é {resultado}')
     case "*":
         resultado = n1 * n2
+        print(f'A multiplicação de {n1} * {n2} é {resultado}')
     case "/":
         if n2 != 0:
             resultado = n1 / n2
+            print(f'A divisão de {n1} / {n2} é {resultado}')
         else:
             resultado = "Erro: não é possível dividir com zero."
     case "_":
-        resultado = "Operação inválida"
+        resultado = "Operação inválida."
 
-print(f'O resultado de {n1} e {n2} é: {resultado}')
+
     
