674ae4a6d473440310d87df365986a608cd54fc9
GustavoFrota <gustavofro90@gmail.com>
2026-04-27 08:46:25 -0300

    Fazendo uma calculadora

---
+++ Calculadora.py
@@ -0,0 +1,21 @@
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+resultado = input("Digite a operação: ")
+
+match resultado:
+    case "+":
+        resultado = n1 + n2
+    case "-":
+        resultado = n1 + n2
+    case "*":
+        resultado = n1 * n2
+    case "/":
+        if n2 != 0:
+            resultado = n1 / n2
+        else:
+            resultado = "Erro: não é possível dividir com zero."
+    case "_":
+        resultado = "Operação inválida"
+
+print(f'O resultado de {n1} {resultado} {n2} é: {resultado}')
+    
