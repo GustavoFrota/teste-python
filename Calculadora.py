5c5a8bdc4819485739c736ea99fc98a395f0544e
GustavoFrota <gustavofro90@gmail.com>
2026-04-29 09:11:29 -0300

    Inicialização e encerramento da calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,4 @@
+print("Iniciando a calculadora")
 n1 = float(input("Digite o primeiro número: "))
 operacao = input("Digite a operação: ")
 n2 = float(input("Digite o segundo número: "))
@@ -7,20 +8,25 @@ match operacao:
     case "+":
         resultado = n1 + n2
         print(f'A soma de {n1} + {n2} é {resultado}')
+        print("Encerrando a calculadora")
     case "-":
         resultado = n1 - n2
         print(f'A subtração de {n1} - {n2} é {resultado}')
+        print("Encerrando a calculadora")
     case "*":
         resultado = n1 * n2
         print(f'A multiplicação de {n1} * {n2} é {resultado}')
+        print("Encerrando a calculadora")
     case "/":
         if n2 != 0:
             resultado = n1 / n2
             print(f'A divisão de {n1} / {n2} é {resultado}')
+            print("Encerrando a calculadora")
         else:
             print("Erro: não é possível dividir com zero.")
     case "**":
         resultado = n1 ** n2
         print(f'A expexponenciação de {n1} ** {n2} é {resultado}')
+        print("Encerrando a calculadora")
     case _:
         print("Operação inválida.")
