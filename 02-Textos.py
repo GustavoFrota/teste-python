9eedbd45ab786fcc81da4a739dc8eda87ea30bd3
GustavoFrota <gustavofro90@gmail.com>
2026-06-11 09:59:36 -0300

    Formatando texto

--- 02-Textos.py
+++ 02-Textos.py
@@ -1,6 +1,6 @@
 faturamento = 1000;
 custo = 600;
 lucro = faturamento - custo;
-texto = "O lucro foi de " + str(lucro) + " e o faturamento foi de " + str(faturamento);
+texto = f"O lucro foi de {lucro} e o faturamento foi de {faturamento}";
 
 print(texto);
