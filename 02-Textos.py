38638ce229f90b291faa880a9caeddbcc154e0ed
GustavoFrota <gustavofro90@gmail.com>
2026-06-11 14:24:48 -0300

    Fazendo alterações e adicionando len

--- 02-Textos.py
+++ 02-Textos.py
@@ -6,5 +6,8 @@ print(texto);
 
 email = " EMAIL_FALSO@gmail.com ";
 
-print(email.lower()) #lower converter todas as letras de uma string para minúsculas
-print(email.strip()) #strip é usado para remover espaços em branco
+email = email.lower(); #lower converter todas as letras de uma string para minúsculas
+email = email.strip(); #strip é usado para remover espaços em branco
+print(email);
+
+print(len(email)) #len é usada para contar a quantidade de elementos
