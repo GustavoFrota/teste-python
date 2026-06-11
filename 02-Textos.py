a559c627ce88941e80349ef68d1dca8f69adfea8
GustavoFrota <gustavofro90@gmail.com>
2026-06-11 14:18:25 -0300

    Adicionando lower e strip

--- 02-Textos.py
+++ 02-Textos.py
@@ -2,5 +2,9 @@ faturamento = 1000;
 custo = 600;
 lucro = faturamento - custo;
 texto = f"O lucro foi de {lucro} e o faturamento foi de {faturamento}";
-
 print(texto);
+
+email = " EMAIL_FALSO@gmail.com ";
+
+print(email.lower()) #lower converter todas as letras de uma string para minúsculas
+print(email.strip()) #strip é usado para remover espaços em branco
