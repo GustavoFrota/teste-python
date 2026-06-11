6c2b8e9aabb2653b4a8a712b72595d1f9c22bc1f
GustavoFrota <gustavofro90@gmail.com>
2026-06-11 09:18:39 -0300

    Adicionando imposto

--- 01-Variáveis_e_operacoes.py
+++ 01-Variáveis_e_operacoes.py
@@ -1,6 +1,8 @@
 faturamento = 1000;
 custo = 600;
+imposto = 0.15 * faturamento;
 
 print("Faturamento", faturamento);
 print("Custo", custo);
-print("Lucro", faturamento - custo);
+print("Lucro", faturamento - custo - imposto);
+print("Imposto", imposto);
