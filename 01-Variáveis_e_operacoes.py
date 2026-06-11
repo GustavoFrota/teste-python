e136c1e4dd7a5c84f6436e4032a03628967aeac4
GustavoFrota <gustavofro90@gmail.com>
2026-06-11 09:31:13 -0300

    Adicionando margem de lucro

--- 01-Variáveis_e_operacoes.py
+++ 01-Variáveis_e_operacoes.py
@@ -1,8 +1,12 @@
 faturamento = 1000;
 custo = 600;
 imposto = 0.15 * faturamento;
+lucro = faturamento - custo - imposto;
 
 print("Faturamento", faturamento);
 print("Custo", custo);
 print("Lucro", faturamento - custo - imposto);
 print("Imposto", imposto);
+
+margem_lucro = lucro / faturamento
+print("Margem", margem_lucro)
