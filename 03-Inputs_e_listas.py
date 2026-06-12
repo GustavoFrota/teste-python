6257f1b341a6def595b593295fe3232234062fbd
GustavoFrota <gustavofro90@gmail.com>
2026-06-12 09:31:29 -0300

    Adicionando input

---
+++ 03-Inputs_e_listas.py
@@ -0,0 +1,4 @@
+faturamento = input("Preencha com o faturamento (apenas números): "); #input é usada para receber dados digitados pelo usuário pelo teclado
+texto = f"O faturamento foi {faturamento}";
+
+print(texto);
