b8e1625e4322c27fc4a53ecc9774ef2f19d337d6
GustavoFrota <gustavofro90@gmail.com>
2026-06-12 09:36:29 -0300

    Adicionando float

--- 03-Inputs_e_listas.py
+++ 03-Inputs_e_listas.py
@@ -1,4 +1,8 @@
 faturamento = input("Preencha com o faturamento (apenas números): "); #input é usada para receber dados digitados pelo usuário pelo teclado
+faturamento = float(faturamento);
+custo = 600;
+lucro = faturamento - custo;
 texto = f"O faturamento foi {faturamento}";
 
 print(texto);
+print(lucro);
