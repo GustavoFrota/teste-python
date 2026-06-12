5975c810a7fe0c1e6706b6758bd780bf991fe367
GustavoFrota <gustavofro90@gmail.com>
2026-06-12 14:02:26 -0300

    Adicionando média

--- 04-Lista.py
+++ 04-Lista.py
@@ -2,5 +2,11 @@ vendas = [100, 50, 1000, 800, 35]
 
 print(vendas[0])
 
+qtde_vendas = len(vendas);
+
 total_vendas = sum(vendas) #sum é usada para somar os elementos
 print(total_vendas)
+
+media = total_vendas / qtde_vendas
+
+print(media);
