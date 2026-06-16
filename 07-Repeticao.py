e0d76955635e1b462479c9522000d40bac65b790
GustavoFrota <gustavofro90@gmail.com>
2026-06-16 15:10:05 -0300

    Adicionando if e else na repetição

--- 07-Repeticao.py
+++ 07-Repeticao.py
@@ -3,8 +3,21 @@ for i in range(10):
 
 lista_precos = [1500, 1000, 800, 2000]
 
-taxa_imposto = 0.1
+#taxa_imposto = 0.1
+
+#for preco in lista_precos:
+#   imposto = preco * taxa_imposto
+#     print(f"Preço do produto {preco}, imposto do produto {imposto}")
+
+total_imposto = 0
 
 for preco in lista_precos:
-    imposto = preco * taxa_imposto
+    if preco > 1000:
+        taxa = 0.15
+    else:
+        taxa = 0.1
+    imposto = taxa * preco
+    total_imposto += imposto
     print(f"Preço do produto {preco}, imposto do produto {imposto}")
+
+print(f"O total de imposto é: {total_imposto}")
