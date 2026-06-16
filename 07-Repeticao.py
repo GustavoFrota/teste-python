98bd6ea2c9a634e77936cf2b26d30569d0907b76
GustavoFrota <gustavofro90@gmail.com>
2026-06-16 13:59:48 -0300

    For com lista

--- 07-Repeticao.py
+++ 07-Repeticao.py
@@ -1,2 +1,10 @@
 for i in range(10):
     print("testando")
+
+lista_precos = [1500, 1000, 800, 2000]
+
+taxa_imposto = 0.1
+
+for preco in lista_precos:
+    imposto = preco * taxa_imposto
+    print(f"Preço do produto {preco}, imposto do produto {imposto}")
