ae482b6ae92624014b897116045627d6059c43a3
GustavoFrota <gustavofro90@gmail.com>
2026-06-17 10:02:15 -0300

    Atualizando chatbot

--- Chat_bot.py
+++ Chat_bot.py
@@ -1,10 +1,18 @@
 while True:
     mensagem = input("Você: ").lower()
 
-    if mensagem == "oi":
-        print("Bot: Olá, como posso ajudar?")
+    if mensagem == "oi" or mensagem == "olá" or mensagem == "ola":
+        print("GSO: Olá, como posso ajudar?")
+
     elif mensagem == "como você está":
-        print("bot: estou funcionando perfeitamente")
+        print("GSO: estou funcionando perfeitamente")
+    
+    elif mensagem == "quem é você":
+        print("GSO: me chamo GSO, um chatbot criado por Gustavo com respostas limitadas")
+    
+    elif mensagem == "sair":
+        print("GSO: até logo.")
         break
+
     else:
-        print("bot: desculpe, não entendi sua pergunta.")
+        print("GSO: desculpe, não entendi sua pergunta.")
