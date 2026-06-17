606e26b4c9ab392f89f47757d545309c92bc6701
GustavoFrota <gustavofro90@gmail.com>
2026-06-17 09:03:34 -0300

    Adicionando uma função

--- 09-Tuplas.py
+++ 09-Tuplas.py
@@ -1,2 +1,11 @@
 tupla_vendas = (1000, 800, 500)
 print(tupla_vendas[0])
+
+def calcular_bonus(lista_vendas):
+    bonus1 = 2 * len(lista_vendas)
+    bonus2 = 0.1 * sum(lista_vendas)
+    return bonus1, bonus2
+
+vendas = [100, 200, 400, 1000]
+resultado = calcular_bonus(vendas)
+print(resultado)
