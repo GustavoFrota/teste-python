9a548efecc2eed9c36705bc753e3cfa32bb145f0
GustavoFrota <gustavofro90@gmail.com>
2026-08-06 13:58:19 -0300

    Melhorando a formatação

--- Dinheiro.py
+++ Dinheiro.py
@@ -1,5 +1,5 @@
 def calcular():
-    saldo = float(input("Digite o seu saldo: "))
+    saldo = float(input("Digite o seu saldo: R$"))
 
     Valor_50 = (saldo * 0.50)
     valor_20 = (saldo * 0.20)
@@ -8,19 +8,19 @@ def calcular():
     valor_5 = (saldo * 0.05)
 
     print("-" * 40)
-    print(f"50% de {saldo} é: {Valor_50}$")
+    print(f"50% de {saldo} é: R${Valor_50:.2f}")
 
     print("-" * 40)
-    print(f"20% de {saldo} é: {valor_20}$")
+    print(f"20% de {saldo} é: R${valor_20:.2f}")
 
     print("-" * 40)
-    print(f"15% de {saldo} é {valor_15}$")
+    print(f"15% de {saldo} é R${valor_15:.2f}")
 
     print("-" * 40)
-    print(f"10% de {saldo} é {valor_10}$")
+    print(f"10% de {saldo} é R${valor_10:.2f}")
 
     print("-" * 40)
-    print(f"5% de {saldo} é {valor_5}")
+    print(f"5% de {saldo} é R${valor_5:.2f}")
 
 
 calcular()
