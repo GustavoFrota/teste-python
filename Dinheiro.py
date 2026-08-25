1f6726ef4b8879e8909777b97f50451334539bb6
GustavoFrota <gustavofro90@gmail.com>
2026-08-25 14:12:00 -0300

    Pequena alteração

--- Dinheiro.py
+++ Dinheiro.py
@@ -9,14 +9,14 @@ def calcular():
     porcentagens = [50, 20, 15, 10, 5]
     distribuicoes = ['Gastos essenciais', 'Sonho de consumo', 'Gastos não essenciais', 'Reserva', 'Investimento']
 
-    print('-' * 40)
+    print('=' * 40)
 
     print("ORGANIZANDO RENDA")
 
     for porcentagem, distribuicao in zip(porcentagens, distribuicoes):
           valor = saldo * porcentagem / 100
 
-          print('-' * 40)
+          print('=' * 40)
           print(distribuicao)
           print(f"{porcentagem}% de R${saldo:.2f} --> R${valor:.2f}")
 
