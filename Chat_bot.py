9f99ba8d331b4a105e4cbd6eb14f2bfe582814be
GustavoFrota <gustavofro90@gmail.com>
2026-08-27 13:35:06 -0300

    Ideia de calcular algo

--- Chat_bot.py
+++ Chat_bot.py
@@ -14,6 +14,10 @@ def chatbot():
     
      case "quem é você"| "quem é voce" | "quem é vc":
         print("GSO: me chamo GSO, um chatbot criado por Gustavo com respostas limitadas")
+
+     case 'calcular':
+          print('GSO: calculando...') #Em andamento
+          
     
      case "sair" | "tchau":
         print("GSO: Programa encerrado, até logo.")
